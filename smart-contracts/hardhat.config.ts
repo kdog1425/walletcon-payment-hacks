0ecf77c499d6e4f9a7b89b74528ae71870a1139f
jTanG0506 <mailto.jtang@gmail.com>
2024-03-22 10:33:42 +0000

    chore: add hardhat config

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -2,8 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
+const ETHERSCAN_API_KEY = "";
+const DEPLOYER_PRIVATE_KEY = "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://rpc.ankr.com/eth_sepolia`,
+      accounts: [DEPLOYER_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
