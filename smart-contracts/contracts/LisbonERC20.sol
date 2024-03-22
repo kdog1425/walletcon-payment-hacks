a2fce7130f58a0c9bc9e32c8d127003dedc07f06
jTanG0506 <mailto.jtang@gmail.com>
2024-03-22 10:33:27 +0000

    chore: remove Ownable on LisbonERC20

--- smart-contracts/contracts/LisbonERC20.sol
+++ smart-contracts/contracts/LisbonERC20.sol
@@ -2,15 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "./ERC20PaymentPermit.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LisbonERC20 is ERC20PaymentPermit, Ownable {
+contract LisbonERC20 is ERC20PaymentPermit {
     constructor(
         string memory name,
         string memory symbol
     ) ERC20(name, symbol) ERC20PaymentPermit(name) {}
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
 }
